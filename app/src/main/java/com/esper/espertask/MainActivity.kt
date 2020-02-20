61ebc97404ac5fb610302e1e7a2156d05ccf15c8
Sachin Verma <dev.sachinverma@hotmail.com>
2020-02-20 16:32:40 +0530

    Initial commit

---
+++ app/src/main/java/com/esper/espertask/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.esper.espertask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun justRun(runnable: Runnable) = runnable.run()
+}
